b96db5113c3147b869c27954cf9d9cc12fb2b541
Verdient <fagui.zhu@foxmail.com>
2024-03-15 15:21:30 +0800

    生产者实现方式调试

--- src/Producer.php
+++ src/Producer.php
@@ -3,27 +3,29 @@
 namespace Verdient\Hyperf3\Amqp;
 
 use Hyperf\Amqp\Message\ProducerMessageInterface;
-use Hyperf\Amqp\Producer;
+use Hyperf\Amqp\Producer as AmqpProducer;
 use Hyperf\Context\ApplicationContext;
 
 /**
- * 包含生产者
+ * 生产者
  * @author Verdient。
  */
-trait HasProducer
+class Producer
 {
     /**
      * 生产消息
      * @param ProducerMessageInterface|ProducerMessageInterface[] $message 消息
      * @param bool $confirm 消息是否需要确认
      * @param int $timeout 超时时间
-     * @return bool
      * @author Verdeint。
      */
-    protected function produce(ProducerMessageInterface|array $message, bool $confirm = true, int $timeout = 5)
-    {
-        /** @var Producer */
-        $producer = ApplicationContext::getContainer()->get(Producer::class);
+    public static function produce(
+        ProducerMessageInterface|array $message,
+        bool $confirm = true,
+        int $timeout = 5
+    ): bool {
+        /** @var AmqpProducer */
+        $producer = ApplicationContext::getContainer()->get(AmqpProducer::class);
         if (is_array($message)) {
             $isOK = true;
             foreach ($message as $message2) {
